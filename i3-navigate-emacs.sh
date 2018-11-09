8a3d8b3787426ff3093847c65d49024e2bad33fe
Yorick Sijsling <yorick@channable.com>
2018-11-09 16:30:23 +0100

    Emacs + I3: Fix navigation issue
    I was depending on windmove to throw an exception if it couldn't move to another window. Only then the
    fallback on i3-move would be used. The exception would sometimes leave emacs in a weird state.

--- i3-navigate-emacs.sh
+++ i3-navigate-emacs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+function debuginfo()
+{
+    # echo "$1" >> ~/i3-nav-emacs-log
+    return 0
+}
+
 function get-focussed-window()
 {
     i3-msg -t get_tree | jq -r ".. | select(.focused? == true).window_properties.class"
@@ -7,24 +14,32 @@ function get-focussed-window()
 
 function i3-move()
 {
+    debuginfo "i3-move \"$1\""
     i3-msg focus "$1"
 }
 
 function emacs-move()
 {
-    emacsclient -e "(windmove-$1)"
+    debuginfo "emacs-move \"$1\""
+
+    # This returns "nil" if it fails, or something like "#<window 3 on yorick.el>" if it succeeds
+    result=$(emacsclient -e "(ignore-errors (windmove-$1))")
+
+    if [[ $result == "nil" ]]; then
+        debuginfo "failed"
+        return 1
+    else
+        debuginfo "OK - moved within emacs"
+        return 0
+    fi
 }
 
 function perform-move()
 {
     focussed_workspace=$(get-focussed-window)
-    if [ "$focussed_workspace" = "Emacs" ]; then
-        emacs-move "$1"
-        result=$?
-        # emacsclient -e "(message \"$result\")"
-        if [ $result -ne 0 ]; then
-            i3-move "$1"
-        fi
+    debuginfo "focus = $focussed_workspace"
+    if [[ "$focussed_workspace" == "Emacs" ]]; then
+        emacs-move "$1" || i3-move "$1"
     # elif [ "$focussed_workspace" = "Firefox" ]; then
     #     case "$1" in
     #         left)
@@ -36,6 +51,7 @@ function perform-move()
     else
         i3-move "$1"
     fi
+    debuginfo ""
 }
 
 case "$1" in

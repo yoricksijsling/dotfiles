58c71785362a54ad5498db4eb47f1bf3d903a130
Yorick Sijsling <yorick@channable.com>
2020-02-27 12:47:25 +0100

    I3: Fix bug in emacs/i3 navigation

--- i3-navigate-emacs.sh
+++ i3-navigate-emacs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Based on https://gist.github.com/mijoharas/b9d09daed9654ca8d0d081015209ecd0
 
 function debuginfo()
 {
@@ -7,9 +8,13 @@ function debuginfo()
     return 0
 }
 
-function get-focussed-window()
+function get-focused-window-class()
 {
     i3-msg -t get_tree | jq -r ".. | select(.focused? == true).window_properties.class"
+}
+function get-focused-window()
+{
+    i3-msg -t get_tree | jq -r ".. | select(.focused? == true).window"
 }
 
 function i3-move()
@@ -18,15 +23,21 @@ function i3-move()
     i3-msg focus "$1"
 }
 
-function emacs-move()
+function emacs-move()  # window_id, direction
 {
-    debuginfo "emacs-move \"$1\""
+    debuginfo "emacs-move \"$1\" \"$2\""
 
-    # This returns "nil" if it fails, or something like "#<window 3 on yorick.el>" if it succeeds
-    result=$(emacsclient -e "(ignore-errors (windmove-$1))")
+    # If there are multiple emacs frames on one screen, emacsclient picks the one under the mouse
+    # cursor. That's not always the currently focused window. Luckily we have a window id from i3
+    # that we can use to find the right frame. Requires dash.
+    elisp_selected_frame="(--first (string-equal \"$1\" (frame-parameter it 'outer-window-id)) (frame-list))"
+    elisp_move="(with-selected-frame $elisp_selected_frame (windmove-$2))"
+
+    # This returns "nil" if it fails, or something like "#<window 3 on foo.el>" if it succeeds
+    result=$(emacsclient -e "(ignore-errors $elisp_move)")
 
     if [[ $result == "nil" ]]; then
-        debuginfo "failed"
+        debuginfo "emacs-move failed"
         return 1
     else
         debuginfo "OK - moved within emacs"
@@ -36,18 +47,14 @@ function emacs-move()
 
 function perform-move()
 {
-    focussed_workspace=$(get-focussed-window)
-    debuginfo "focus = $focussed_workspace"
-    if [[ "$focussed_workspace" == "Emacs" ]]; then
-        emacs-move "$1" || i3-move "$1"
-    # elif [ "$focussed_workspace" = "Firefox" ]; then
-    #     case "$1" in
-    #         left)
-    #             wmctrl -a firefox; xdotool --key Ctrl+Shift+Tab;;
-    #         right)
-    #             wmctrl -a firefox; xdotool key Ctrl+Tab;;
-    #         *) i3-move "$1";;
-    #     esac
+    focused_window_class=$(get-focused-window-class)
+    focused_window=$(get-focused-window)
+
+    debuginfo "focused_window_class = $focused_window_class"
+    debuginfo "focused_window = $focused_window"
+
+    if [[ "$focused_window_class" == "Emacs" ]]; then
+        emacs-move "$focused_window" "$1" || i3-move "$1"
     else
         i3-move "$1"
     fi

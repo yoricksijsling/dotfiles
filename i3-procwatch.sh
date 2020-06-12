e3d8808631f4d59df6956d8faf1de9c415ae8f6c
Yorick Sijsling <yorick@channable.com>
2020-06-12 17:14:12 +0200

    I3: Procwatch interactive

--- i3-procwatch.sh
+++ i3-procwatch.sh
@@ -34,8 +34,57 @@ function watch()
     done
 }
 
+function list-potentials()
+{
+    local current_tty
+    current_tty=$(tty | tail -c+6)
+
+    local awkscript='
+      BEGIN {
+        FIELDWIDTHS = "7 8 13 20"
+      }
+      {
+        tty = $2
+        # Started within the last 24 hours:
+        # started_recently = $3~"^ +([0-9][0-9]:)?[0-9][0-9]:[0-9][0-9] $"
+        # Started within the last 9999 seconds (almost 3 hours):
+        started_recently = $3~"^ +[0-9]{1,4} $"
+        comm = $4
+        if (tty  ~ "^pts" &&
+            tty !~ "^"current_tty &&
+            tty != printed_tty &&
+            started_recently &&
+            comm !~ "^bash") {
+          print $0
+          printed_tty=$2  # Only print the first non-bash process in a tty
+        }
+      }
+      '
+    ps a --no-headers --sort=-tty,-etime -o pid:6,tty:7,etimes:12,comm:19 \
+        | awk -v current_tty="$current_tty" "$awkscript" \
+        | sort -k3
+}
+
+function interactive()
+{
+    local potentials
+    readarray -t potentials <<< "$(list-potentials)"
+
+    select choice in "${potentials[@]}"; do
+        local pid
+        pid=$(echo "$choice" | head -c 7 | tr -d ' ')
+        if [[ "$choice" =~ [0-9]+ ]]; then
+            register "$pid"
+        else
+            echo "Invalid choice"
+        fi
+        exit
+    done
+}
+
 case "$1" in
     "register") register "$2";;
     "watch") watch;;
-    *) exit 1;;
+    "list-potentials") list-potentials;;
+    *) interactive;;
 esac

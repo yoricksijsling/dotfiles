c41aa43fd2c9003c3ae8c6134865f62383cc3d76
Yorick Sijsling <yorick@channable.com>
2020-06-12 17:04:33 +0200

    I3: Use rofi instead of dmenu

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# Prompt the user for some input using dmenu
+# Prompt the user for some input using rofi
 # ung.py does some channable-specific url shortening
-content_ung=$(dmenu -p "Todo in #Work" <&- | ~/channable/scripts/ung.py)
+content_ung=$(rofi -dmenu -p "Todo" -filter "#Work tod " -lines 0 -width 90 | ~/channable/scripts/ung.py)
 
 # Content will be empty if the user cancelled
 if [[ ! -z "$content_ung" ]]; then
 
     # Relies on todoist go app. See note 'Todoist CLI'.
-    ~/opensource/todoist/bin/todoist quick "#Work today $content_ung"
+    ~/opensource/todoist/bin/todoist quick "$content_ung"
 
 fi
 

6a9b226e3b8202d9617a9ae04b906df78413438a
Yorick Sijsling <yorick@channable.com>
2021-11-04 11:56:12 +0100

    Emacs: Use web-mode for php

--- emacs/languages.el
+++ emacs/languages.el
@@ -42,3 +42,27 @@
              '(js-mode "{" "}" "/[*/]" nil))
 (add-hook 'js-mode-hook 'hs-minor-mode)
 (define-key js-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
+
+;;--------------------------------------------------------------------------------
+;; HTML / PHP
+
+;; https://web-mode.org/
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq-local tab-width 4)))
+(add-to-list 'ahs-modes 'web-mode)
+
+;; Other options:
+;; js, jsx, css, scss, xml
+;;(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+

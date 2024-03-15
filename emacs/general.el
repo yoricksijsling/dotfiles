ee44289b3be93c5eabe1ab1b9c71123bf219e47b
Yorick Sijsling <yorick@channable.com>
2024-03-15 13:00:46 +0100

    Emacs: add eval-and-replace function for elisp

--- emacs/general.el
+++ emacs/general.el
@@ -476,7 +476,15 @@ _q_uit                         _t_: Show subtree
     (beginning-of-line)))
 (global-set-key (kbd "C-a") 'beginning-of-line-or-indentation)
 
-
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
 
 ;; For multiple cursors: sorting lines by region
 (defvar mc--lists-to-replace nil)
